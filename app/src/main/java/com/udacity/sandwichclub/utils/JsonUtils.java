0d0114d6beb54b4e8eb8aee1604578e5a1db75f8
bubal93 <bublik.alexander93@gmail.com>
2020-05-16 21:53:04 +0200

    Implemented JSON parsing. Gradle fixes.

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,44 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+
+        JSONObject sandwich = new JSONObject(json);
+        JSONObject name = sandwich.getJSONObject("name");
+
+        String mainName = name.getString("mainName");
+
+        JSONArray alsoKnownAs = sandwich.getJSONArray("alsoKnownAs");
+        List<String> knownAs = getStringList(alsoKnownAs);
+
+        String placeOfOrigin = sandwich.getString("placeOfOrigin");
+
+        String description = sandwich.getString("description");
+
+        String image = sandwich.getString("image");
+
+        JSONArray ingredientsArray = sandwich.getJSONArray("ingredients");
+        List<String> ingredients = getStringList(ingredientsArray);
+
+        return new Sandwich(mainName, knownAs, placeOfOrigin, description, image, ingredients);
+    }
+
+    private static List<String> getStringList(JSONArray array) throws JSONException {
+        List<String> strings = new ArrayList<>();
+
+        for (int i = 0; i < array.length(); i++) {
+            strings.add(array.getString(i));
+        }
+
+        return strings;
     }
 }
